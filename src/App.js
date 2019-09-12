b44d163ed8a2a5481337cc9d46309e2ac4cd8b66
Ale <alearanibar@gmail.com>
2019-09-12 16:14:34 -0700

    css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./App.css";
 import Galaxy from './components/Galaxy'
-import Asteroids from './components/Asteroids'
+import styled from "styled-components";
 
 
+const MainP = styled.div`
+background-color: #6363b1;
+`
 function App() {
   return (
-    <div className="App">
+    <MainP className="App">
       {<p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>}
       <Galaxy />
       
-    </div>
+    </MainP>
   );
 }
 
--- src/components/Galaxy.js
+++ src/components/Galaxy.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Asteroids from "./Asteroids";
+import { Container, Row } from "reactstrap";
+import styled from "styled-components";
+
+const nasaKey = 'SUMQETBpXGEjrw20RdOmp6e2R0CqSbtCvMaLeQsw';
 
 export default function Galaxy() {
   const [galaxies, setGalaxies] = useState([]);
 
+
+  const HeadLine = styled.h1`
+  text-shadow: 1px 1px 2px red, 0 0 1em blue, 0 0 0.2em blue;
+  font-size: 4rem;
+
+  `
+
   useEffect(() => {
     axios
-      .get(`https://api.nasa.gov/planetary/apod?api_key=SUMQETBpXGEjrw20RdOmp6e2R0CqSbtCvMaLeQsw`)
+      .get(`https://api.nasa.gov/planetary/apod?api_key=${nasaKey}`)
       .then(response => {
         const galaxies = response.data;
         console.log(`new data`, galaxies);
@@ -19,17 +30,19 @@ export default function Galaxy() {
       });
   }, []);
   return (
-    <div>
-        <h1>Nasa Explorer</h1>
+    <Container>
+      <Row>
+        <HeadLine>And todays image is: </HeadLine>
       <Asteroids
         key={galaxies.index}
         date={galaxies.date}
         img={galaxies.hdurl}
         description={galaxies.explanation}
         copyright={galaxies.copyright}
+        title={galaxies.title}
       />
-      
-    </div>
+      </Row>
+    </Container>
   )
 }
 
